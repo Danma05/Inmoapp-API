db77383dbb32eb65067e223256b2e60e6f35ed34
Danma05 <161197938+Danma05@users.noreply.github.com>
2025-11-14 12:41:42 -0500

    updateee

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js – API completa con frontend + pg + reintentos + healthcheck
+// index.js – API completa con frontend + pg + reintentos + healthcheck.
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
